8036d5a4b9d1b5d9eb05108dfb877dfb6158f8cb
Aditya Kushwaha <kushwahaaditya251@gmail.com>
2021-07-12 00:08:38 +0530

    adding controoler and route s

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const express = require ( "express") ;
 const app = express() ; 
 const db = require ( "./db")
 app.set ( "view engine" , "hbs") ; 
+app.use ( "/" , require ("./routes")) ; 
 
-app.get ( "/" , (req, res)=>{
+
+/* app.get ( "/" , (req, res)=>{
     return  res.redirect("index") ; 
 })
 app.get ( "/index", (req, res)=>{
     return res.render ( "index") ; 
-})
+}) */
 app.listen ( 2424 , ()=>{
     console.log("server started on http://localhost:2424") ; 
 })
